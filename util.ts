81dc958f831374eb55ef840feffc977cfae2f813
Christopher Brown <io@henrian.com>
2015-05-15 15:55:34 -0500

    Add pushAll, flatten, and flatMap to util

--- util.ts
+++ util.ts
@@ -1,5 +1,20 @@
 export const log = console.log.bind(console);
 
+// pushAll, flatten, and flatMap come from pdfi/Arrays
+
+export function pushAll<T>(array: T[], items: T[]): void {
+  return Array.prototype.push.apply(array, items);
+}
+
+export function flatten<T>(arrays: T[][]): T[] {
+  return Array.prototype.concat.apply([], arrays);
+}
+
+export function flatMap<T, R>(elements: T[], callback: (element: T, index: number, array: T[]) => R[], thisArg?: any): R[] {
+  const arrays: R[][] = elements.map(callback, thisArg);
+  return flatten(arrays);
+}
+
 /**
 Search the codebase for @util.memoize or @memoize for usage examples.
 */
