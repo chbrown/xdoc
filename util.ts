0201a2972a57b6a01bec314708e20f47da64955f
Christopher Brown <io@henrian.com>
2015-05-15 14:49:14 -0500

    Add memoize decorator from pdfi

--- util.ts
+++ util.ts
@@ -1,1 +1,19 @@
 export const log = console.log.bind(console);
+
+/**
+Search the codebase for @util.memoize or @memoize for usage examples.
+*/
+export function memoize<T>(target: Object,
+                           propertyKey: string,
+                           descriptor: TypedPropertyDescriptor<T>) {
+  var get = descriptor.get;
+  var memoizedPropertyKey = `_memoized_${propertyKey}`;
+  descriptor.get = function() {
+    var got = memoizedPropertyKey in this;
+    if (!got) {
+      this[memoizedPropertyKey] = get.call(this);
+    }
+    return this[memoizedPropertyKey];
+  }
+  return descriptor;
+}
