dc71ea42d788d0ab6e9a674fe8725673de7b236e
Christopher Brown <io@henrian.com>
2015-08-25 15:20:45 -0500

    Add react type declarations

--- type_declarations/index.d.ts
+++ type_declarations/index.d.ts
@@ -1,10 +1,13 @@
 /// <reference path="DefinitelyTyped/angularjs/angular.d.ts" />
 /// <reference path="DefinitelyTyped/jszip/jszip.d.ts" />
 /// <reference path="DefinitelyTyped/lodash/lodash.d.ts" />
+/// <reference path="DefinitelyTyped/react/react.d.ts" />
 /// <reference path="DefinitelyTyped/virtual-dom/virtual-dom.d.ts" />
 
 /// <reference path="../node_modules/adts/adts.d.ts" />
+/// <reference path="../node_modules/arrays/arrays.d.ts" />
 /// <reference path="../node_modules/coders/coders.d.ts" />
-/// <reference path="../node_modules/xmltree/xmltree.d.ts" />
+/// <reference path="../node_modules/notify-ui/notify-ui.d.ts" />
 
 interface Map<V> { [index: string]: V }
+declare var require;
