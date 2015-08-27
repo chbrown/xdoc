ec6d2a4730aa1d36211dd02cc7ff702ce2c90467
Christopher Brown <io@henrian.com>
2015-08-27 11:27:20 -0500

    Add type declarations for testing

--- type_declarations/index.d.ts
+++ type_declarations/index.d.ts
@@ -1,6 +1,7 @@
 /// <reference path="DefinitelyTyped/angularjs/angular.d.ts" />
 /// <reference path="DefinitelyTyped/jszip/jszip.d.ts" />
 /// <reference path="DefinitelyTyped/lodash/lodash.d.ts" />
+/// <reference path="DefinitelyTyped/mocha/mocha.d.ts" />
 /// <reference path="DefinitelyTyped/react/react.d.ts" />
 /// <reference path="DefinitelyTyped/virtual-dom/virtual-dom.d.ts" />
 
@@ -10,4 +11,12 @@
 /// <reference path="../node_modules/notify-ui/notify-ui.d.ts" />
 
 interface Map<V> { [index: string]: V }
-declare var require;
+
+// from node.d.ts
+interface NodeRequireFunction {
+    (id: string): any;
+}
+interface NodeRequire extends NodeRequireFunction {
+    resolve(id:string): string;
+}
+declare var require: NodeRequire;
