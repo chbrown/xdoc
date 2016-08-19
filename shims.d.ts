e1c138526ae6a12d667c8fce44b02e0bff8e6879
Christopher Brown <io@henrian.com>
2016-08-19 11:10:24 -0500

    Remove shims declarations

--- shims.d.ts
+++
@@ -1,8 +0,0 @@
-// from node.d.ts
-interface NodeRequireFunction {
-    (id: string): any;
-}
-interface NodeRequire extends NodeRequireFunction {
-    resolve(id:string): string;
-}
-declare var require: NodeRequire;
